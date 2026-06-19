ca594c672f258c1a394d8e1b036f00f1d6861364
PhoneGram <reda7new@gmail.com>
2026-06-19 20:08:55 +0300

    update

--- master.js
+++ master.js
@@ -1,1 +1,7 @@
-console.log("Hi")
+let title = 'Reda', desc = 'Hello world', time = '25/10';
+
+let card = `<h2>Hello ${title}</h2>
+<p>${desc}</p>
+<span style="color: red;">${time}</span>`;
+
+document.write(card.repeat(4));
